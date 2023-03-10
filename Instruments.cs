bfefac3bfe116750cc7cbf09e032b99ed8500b8b
RJ Bishop <rbishop0033@gmail.com>
2023-03-09 19:07:18 -0500

    Adding finished KC2

---
+++ Instruments.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicalInstruments
+{
+    public class Instrument
+    {
+        public bool percussion;
+        public bool stringed;
+        public bool woodwind;
+        public bool brass;
+
+        public Instrument()
+        {
+            this.percussion = false;
+            this.stringed = false;
+            this.woodwind = false;
+            this.brass = false;
+        }
+    }
+    public class Drums : Instrument
+        {
+
+        public int numberOfCymbals;
+
+        public Drums()
+        {
+          
+            this.percussion= true;
+            this.stringed= false;
+            this.woodwind= false;
+            this.brass= false;
+            this.numberOfCymbals= 0;
+        }
+             
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using MusicalInstruments;
+
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Drums>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var myDrums = new Drums();
+
+    Console.WriteLine("How many cymbals do I have?");
+    myDrums.numberOfCymbals = Convert.ToInt32(Console.ReadLine());
+
+
+    recordList.Add(myDrums);
+        
+    Console.WriteLine("Number of cymbals: " + myDrums.numberOfCymbals);
+}
+
+// Print out the list of records using Console.WriteLine()
